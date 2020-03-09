679060b5bf078cd68ddd8c1031c10d7ba95cfb56
Andri Tanuarto <andritanuarto@gmail.com>
2020-03-09 13:29:17 -0700

    Extract enzyme setup to setupTest.js

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,5 @@
+import checkPropTypes from 'check-prop-types';
+
 /**
  * Functional react component for congratulatory message.
  * @function
@@ -6,4 +8,15 @@
 
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
+}
+
+export const checkProptypes = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name
+  );
+
+  expect(propError).toBeUndefined();
 }
