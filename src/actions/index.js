dcf70f421f98a81ac677ffa98819a95fb7476922
Andri Tanuarto <andritanuarto@gmail.com>
2020-03-11 14:02:03 -0700

    Set up guessWords tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,12 @@
+import GuessedWords from "../GuessedWords";
+
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD'
 };
 
-/**
- * @function correctGuess
- * @returns {object} - Action object with type `CORRECT_GUESS
- */
-export function correctGuess() {
-  return { type: actionTypes.CORRECT_GUESS };
+export const guessWord = (guessedWords) => {
+  return function(dispatch, getState) {
+
+  }
 }
