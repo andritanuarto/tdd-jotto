39ab3f1115a988b2b339acf8653ad98257650db4
Andri Tanuarto <andritanuarto@gmail.com>
2020-03-11 12:09:59 -0700

    Write test for Input success=true

--- src/input.test.js
+++ src/input.test.js
@@ -23,32 +23,42 @@ describe('render', () => {
     });
 
     test('renders components without error', () => {
-      const component = findByTestAttr(wrapper, "component-input");
+      const component = findByTestAttr(wrapper, 'component-input');
       expect(component.length).toBe(1);
     });
 
     test('renders input box', () => {
-      const inputBox = findByTestAttr(wrapper, "input-box");
+      const inputBox = findByTestAttr(wrapper, 'input-box');
       expect(inputBox.length).toBe(1);
     });
 
     test('renders submit button', () => {
-      const submitBUtton = findByTestAttr(wrapper, "submit-button");
+      const submitBUtton = findByTestAttr(wrapper, 'submit-button');
       expect(submitBUtton.length).toBe(1);
     });
   });
 
   describe('word has been guessed', () => {
-    test('renders component without error', () => {
+    let wrapper;
 
+    beforeEach(() => {
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    })
+
+    test('renders component without error', () => {
+      const componentInput = findByTestAttr(wrapper, 'component-input');
+      expect(componentInput.length).toBe(1);
     });
 
     test('does not renders input box', () => {
-
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(0);
     });
 
     test('does not renders submit button', () => {
-
+      const submit = findByTestAttr(wrapper, 'submit-button');
+      expect(submit.length).toBe(0);
     });
   });
 });
