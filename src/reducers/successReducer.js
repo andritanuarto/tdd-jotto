02b0bf67e4c5ac7351b1c3087af158d5dbbe1803
Andri Tanuarto <andritanuarto@gmail.com>
2020-03-10 21:10:56 -0700

    Setup redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+/**
+ * @function successReducer
+ * @param {array} guessedWord - Array of guessed word.
+ * @param {object} secretWord - action to be reduced.
+ * @returns {boolean} - new success state.
+ */
+
+export default (state, action) => {
+  return null;
+}
