8d019df63f9fc876f93004a386510bdf0e9b6eb7
Andri Tanuarto <andritanuarto@gmail.com>
2020-03-13 22:53:36 -0700

    Integration test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,23 @@
 import GuessedWords from "../GuessedWords";
+import { getLetterMatchCount } from '../helpers';
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD'
 };
 
-export const guessWord = (guessedWords) => {
+export const guessWord = (guessedWord) => {
   return function(dispatch, getState) {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
 
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount }
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORRECT_GUESS });
+    }
   }
 }
